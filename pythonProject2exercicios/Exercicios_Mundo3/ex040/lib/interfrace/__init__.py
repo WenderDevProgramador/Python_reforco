ef54368f3f7ca97b95317d3ce431f7fd611fed64
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-06-29 15:58:34 -0300

    hhh

--- pythonProject2exercicios/Exercicios_Mundo3/ex040/lib/interfrace/__init__.py
+++ pythonProject2exercicios/Exercicios_Mundo3/ex040/lib/interfrace/__init__.py
@@ -1,3 +1,23 @@
+
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (TypeError,ValueError,KeyError):
+            print('\033[1;31m ERRO! Digite um número inteiro valido\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('Usuário preferio não inserir valor!')
+            n = 0
+            return n
+            break
+        except:
+            print(f'\033[1;31mERRO ao digitar valor!\033[m')
+            continue
+        else:
+            return n
+
 def linha(tam=42):
     return '-' * tam
     
@@ -8,4 +28,9 @@ def cabeçalho(txt):
     
 def menu(lista):
     cabeçalho('MENU PRINCIPAL')
-    print(lista)
+    for v,item in enumerate(lista):
+        print(f'\033[1;33m{v+1} - \033[0;34m{item}\033[m')
+    print(linha())
+    opc = leiaInt('Digite sua opção: ')
+    
+    return opc
--- pythonProject2exercicios/Exercicios_Mundo3/ex040/sistema.py
+++ pythonProject2exercicios/Exercicios_Mundo3/ex040/sistema.py
@@ -1,6 +1,20 @@
 #Crie um pequeno sistema modularizado que permite cadastrar passoas pelo seu nome e idade em um arquivo de texto simples.
 #O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as pessoas cadastradas.
-from lib import interfrace
+from lib.interfrace import *
+from time import sleep
+
+while True:
+    resposta = menu(['CADASTRAR PESSOAS','VER LISTA DE CADASTRO','SAIR DO SISTEMA'])
+    if resposta == 1:
+        cabeçalho('CADASTRAR PESSOAS')
+    elif resposta == 2:
+        cabeçalho('VER LISTA DE CADASTRO')
+    elif resposta == 3:
+        cabeçalho('OK. Saindo do sitema...')
+        break
+    else:
+        print('\033[1;31mERRO! Opção invalida!\033[m')
+    sleep(1)
+        
 
 
-interfrace.menu(['Antonio','Zé Naldo','Matias'])
