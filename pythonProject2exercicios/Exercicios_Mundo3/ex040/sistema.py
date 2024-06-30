452b3aa969ac0f323d51e2b266c6a081043f4959
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-06-30 18:21:07 -0300

    finish

--- pythonProject2exercicios/Exercicios_Mundo3/ex040/sistema.py
+++ pythonProject2exercicios/Exercicios_Mundo3/ex040/sistema.py
@@ -1,14 +1,26 @@
 #Crie um pequeno sistema modularizado que permite cadastrar passoas pelo seu nome e idade em um arquivo de texto simples.
 #O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as pessoas cadastradas.
 from lib.interfrace import *
+from lib.arquivo import *
 from time import sleep
 
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+    
 while True:
     resposta = menu(['CADASTRAR PESSOAS','VER LISTA DE CADASTRO','SAIR DO SISTEMA'])
     if resposta == 1:
-        cabeçalho('CADASTRAR PESSOAS')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastraPessoa(arq,nome,idade)
+        
     elif resposta == 2:
         cabeçalho('VER LISTA DE CADASTRO')
+        lerArquivo(arq)
     elif resposta == 3:
         cabeçalho('OK. Saindo do sitema...')
         break
